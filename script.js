af0e872bcf5195f0b2a613cc0fd583b6bb4dd909
Juan Carrillo <juan@juanca.org>
2023-01-10 20:10:13 -0600

    fix(refreshbtn): use correct selectors for the refresh animation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // How to trigger a CSS animation on a button click.
 // Credit to dimlucas:
 // https://www.dimlucas.com/index.php/2016/11/17/how-to-trigger-a-css-animation-on-button-click/
-const refreshButton = document.getElementById("refresh");
+const refreshButton = document.getElementById("refresh-icon");
 refreshButton.addEventListener("click", () => {
   refreshButton.classList.add("spin-animation");
   setTimeout(() => {
